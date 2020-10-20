0276431f32d8038a30a09df1da5ff0d2676d263a
Roberto Domínguez <201902@CALASANZSALAMANCA.LOCAL>
2020-10-20 10:12:11 +0200

    DOM

---
+++ TrabajarConDom/javaScripts/trabajarConDom.js
@@ -0,0 +1,29 @@
+/**
+ * Ejercicio 1
+ */
+//Punto 1
+let ej1p1 = document.getElementById("apellido1");
+//Punto 2
+let ej1p2 = document.getElementsByTagName('p');
+//Punto 3
+let ej1p3 = document.getElementById("seccionTercera").getElementsByTagName('p');
+//Punto 4
+let ej1p4 = document.getElementsByTagName('input');//Falta
+//Punto 5
+let ej1p5 = document.getElementsByName("sexo");
+//Punto 6
+let ej1p6 = document.getElementsByClassName("par");
+/**
+ * Ejercicio 2
+ */
+//Punto 1
+let ej2p1 = document.getElementById("seccionPrimera").firstElementChild;
+//Punto 2
+let ej2p2 = document.getElementById("seccionPrimera").lastElementChild;
+//Punto 3
+let ej2p3 = document.getElementById("seccionLista").lastElementChild.lastElementChild;
+//Punto 4
+let ej2p4 = document.getElementsByTagName('input')//Falta
+/**
+ * Ejercicio 3
+ */
