4fff6fbfe01714c8e15d31a70e388f6ea55009aa
Roberto Domínguez <201902@CALASANZSALAMANCA.LOCAL>
2020-10-21 14:12:50 +0200

    gj

--- TrabajarConDom/javaScripts/trabajarConDom.js
+++ TrabajarConDom/javaScripts/trabajarConDom.js
@@ -3,7 +3,7 @@
  */
 //Punto 2.1
 function focalizar() {
-    document.getElementById("nombre").focus;
+    document.getElementById("nombre").focus();
 }
 //Punto 2.2
 function quitarFoco() {
@@ -13,7 +13,12 @@ function quitarFoco() {
  * Ejercicio 4
  */
 //Punto 1
-function crearElemento() {
-    elemento.insertBefore(nuevoNodo, nodo)
+function elementoH2() {
+    var element = document.createElement("h2");
+    var text = document.createTextNode("Lorem Ipsum de nuevo");
+    var padre = document.getElementById("seccionTercera");
+    var hijo = padre.firstElementChild;
+    element.appendChild(text);
+    padre.insertBefore(element, hijo);
 }
 //Punto 2
