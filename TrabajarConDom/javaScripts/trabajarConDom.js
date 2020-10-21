2601ae41e2491ca4b3b76cfc2ac658c121315517
Roberto Domínguez <201902@CALASANZSALAMANCA.LOCAL>
2020-10-21 12:21:13 +0200

    ugjk

--- TrabajarConDom/javaScripts/trabajarConDom.js
+++ TrabajarConDom/javaScripts/trabajarConDom.js
@@ -1,41 +1,18 @@
 /**
- * Ejercicio 1
- */
-//Punto 1
-let ej1p1 = document.getElementById("apellido1");
-//Punto 2
-let ej1p2 = document.getElementsByTagName('p');
-//Punto 3
-let ej1p3 = document.getElementById("seccionTercera").getElementsByTagName('p');
-//Punto 4
-let ej1p4 = document.getElementsByTagName('input');//Falta
-//Punto 5
-let ej1p5 = document.getElementsByName("sexo");
-//Punto 6
-let ej1p6 = document.getElementsByClassName("par");
-/**
- * Ejercicio 2
- */
-//Punto 1
-let ej2p1 = document.getElementById("seccionPrimera").firstElementChild;
-//Punto 2
-let ej2p2 = document.getElementById("seccionPrimera").lastElementChild;
-//Punto 3
-let ej2p3 = document.getElementById("seccionLista").lastElementChild.lastElementChild;
-//Punto 4
-let ej2p4 = document.getElementsByTagName('input')//Falta
-/**
  * Ejercicio 3
  */
-//Punto 1.1
-let ej3p1 = ul.innerHTML.textContent;
-//Punto 1.2
-let ej3p2 
-//Punto 1.3
-let ej3p3
-//Punto 1.4
-let ej3p4
 //Punto 2.1
-let ej3p5
+function focalizar() {
+    document.getElementById("nombre").focus;
+}
 //Punto 2.2
-let ej3p6
+function quitarFoco() {
+    document.getElementById("nombre").blur();
+}
+/**
+ * Ejercicio 4
+ */
+//Punto 1
+function crearElemento() {
+    elemento.insertBefore(nuevoNodo, nodo)
+}
